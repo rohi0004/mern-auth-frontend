e99b14db373af2346b2a1d0695cacf2738235c29
Rohit kumar <sahrohitkumar10@gmail.com>
2025-03-06 00:39:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,jsx}',
@@ -79,4 +79,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
+};
