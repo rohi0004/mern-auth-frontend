c08cf7b65f10444a157a5a9ba25b65caa73304af
Rohit kumar <sahrohitkumar10@gmail.com>
2025-03-06 00:44:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate'; // Import the plugin using ES modules syntax
+
 export default {
   darkMode: ['class'],
   content: [
@@ -78,5 +80,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate], // Use the imported plugin here
 };
